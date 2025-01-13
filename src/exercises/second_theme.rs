c98833554a034bf0d9ffa2a224ae25c16ddde5f9
FunneMonkee <luismiguel11072000@gmail.com>
2025-01-13 20:34:41 +0000

    second theme more exercises

--- src/exercises/second_theme.rs
+++ src/exercises/second_theme.rs
@@ -1,6 +1,11 @@
 use crate::{
     formulas::{
-        eletrical_current::{electromotive_force, velocity_from_current_electron_density_and_area},
+        eletrical_current::{
+            electromotive_force, energy_from_time_and_potency, intensity_from_potency_and_current,
+            intensity_from_resistance_and_potency, potency_from_resistance_and_intensity,
+            resistance_from_current_and_intensity, resistance_from_resistivity,
+            velocity_from_current_electron_density_and_area,
+        },
         geometry::cilinder_area,
     },
     units::unit::Unit,
@@ -11,6 +16,10 @@ use super::helper::answer;
 pub fn solve() {
     println!("Second Theme");
     ex1();
+    ex2();
+    ex3();
+    ex4();
+    ex5();
 }
 
 fn ex1() {
@@ -45,4 +54,80 @@ fn ex2() {
     let charge = 3.;
 
     answer("Ex2", "part 1", electromotive_force(battery, charge), "V");
+}
+
+fn ex3() {
+    let n_days = 15.;
+    let n_lamps = 3.;
+    let hours = 5.;
+    let potency = 100.;
+    let price_per_whatt = Unit {
+        value: 0.0647,
+        prefix: -3,
+        is_grams: false,
+    }
+    .to_base();
+
+    let time = hours * n_days;
+    let potency_sum = potency * n_lamps;
+
+    let total = energy_from_time_and_potency(potency_sum, time) * price_per_whatt;
+    answer("Ex3", "", total, "€");
+}
+
+fn ex4() {
+    let resistance;
+    //b
+    {
+        let potency = 60.;
+        let current = 120.;
+        let intensity = intensity_from_potency_and_current(potency, current);
+        resistance = resistance_from_current_and_intensity(current, intensity);
+
+        answer("Ex4", "b1) intensity", intensity, "A");
+        answer("Ex4", "b2) resistance", resistance, "Ω");
+    }
+    //c
+    {
+        //resistance is immutable so 240Ω
+        answer(
+            "Ex4",
+            "c)",
+            potency_from_resistance_and_intensity(resistance, 0.25),
+            "W",
+        );
+    }
+}
+
+fn ex5() {
+    let radius = Unit {
+        value: 0.362,
+        prefix: -2,
+        is_grams: false,
+    }
+    .to_base()
+        / 2.;
+    let resistivity = Unit {
+        value: 1.72,
+        prefix: -8,
+        is_grams: false,
+    }
+    .to_base();
+    let resistance;
+    //a
+    {
+        resistance = resistance_from_resistivity(resistivity, 1., cilinder_area(radius));
+        answer("Ex5", "a)", resistance, "Ω/m");
+    }
+    //b
+    {
+        answer(
+            "Ex5",
+            "b)",
+            intensity_from_resistance_and_potency(resistance, 3.),
+            "A",
+        );
+    }
+    //c
+    {}
 }
--- src/formulas/eletrical_current.rs
+++ src/formulas/eletrical_current.rs
@@ -11,3 +11,27 @@ pub fn velocity_from_current_electron_density_and_area(
 pub fn electromotive_force(transformed_energy: f64, charge: f64) -> f64 {
     transformed_energy / charge
 }
+
+pub fn energy_from_time_and_potency(potency: f64, time: f64) -> f64 {
+    potency * time
+}
+
+pub fn intensity_from_potency_and_current(potency: f64, current: f64) -> f64 {
+    potency / current
+}
+
+pub fn resistance_from_current_and_intensity(current: f64, intensity: f64) -> f64 {
+    current / intensity
+}
+
+pub fn intensity_from_resistance_and_potency(resistance: f64, potency: f64) -> f64 {
+    (potency / resistance).sqrt()
+}
+
+pub fn potency_from_resistance_and_intensity(resistance: f64, intensity: f64) -> f64 {
+    resistance * intensity.powi(2)
+}
+
+pub fn resistance_from_resistivity(resistivity: f64, length: f64, area: f64) -> f64 {
+    resistivity * length / area
+}
